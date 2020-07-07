0f6cd2e890066a7cebb1cf868898a6674d680e11
Jamie Stivala <jamiestivala@gmail.com>
2020-07-07 20:36:29 +0200

    Added the DiscordClient

---
+++ src/DiscordTools.ts
@@ -0,0 +1,29 @@
+import * as Discord from 'discord.js'
+import * as properties from '../resources/config.json'
+import UsernameCache from "./cache/UsernameCache";
+
+export default class DiscordTools{
+    readonly guildId : string;
+    readonly client : Discord.Client;
+    readonly usernameCache : UsernameCache;
+
+    constructor() {
+        this.guildId = properties.guildId;
+        this.client = new Discord.Client();
+        this.usernameCache = new UsernameCache();
+    }
+
+    async getUsername(userId : string) : Promise <string> {
+        if(!this.usernameCache.isCacheExpired(userId)) return this.usernameCache.userCache.get(userId);
+
+        let guildMember = this.client.guilds.cache.get(this.guildId).members.cache.get(userId);
+        if (guildMember.partial) await guildMember.fetch();
+        this.usernameCache.updateCache(userId, guildMember.nickname);
+
+        return guildMember.nickname;
+    }
+
+    loginClient(){
+        this.client.login(properties.token).then(() => console.log("Bot starting up"));
+    }
+}
