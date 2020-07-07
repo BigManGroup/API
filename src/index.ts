cf77dfa62ace33c5e70f17f2a94670e237aadf04
Jamie Stivala <jamiestivala@gmail.com>
2020-07-07 21:17:34 +0200

    Revamped index

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,40 @@
+import DatabaseWrapper from "./database/DatabaseWrapper";
+import DiscordTools from "./DiscordTools";
 import * as http from "http";
-import {MongoClient} from "mongodb";
-import * as properties from '../resources/config.json'
-import Quote from "./Quote";
-import * as Discord from 'discord.js';
+import {getRandomNumber} from "./Tools";
 
-async function runner() {
-    let clientPool: MongoClient = await new MongoClient(properties.database, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        poolSize: 15,
-        keepAlive: true
-    }).connect();
+class Runner{
+    discordTools : DiscordTools;
+    databaseWrapper : DatabaseWrapper;
+    server : http.Server;
 
-    let server = http.createServer(async (req, res) => {
-        let quotes = await getApprovedQuotes(clientPool);
-        let quote =  (Array.from(quotes.values()))[getRandomNumber(0, quotes.size-1)];
-
-        let formattedQuote = quote.quoteText + " - " + quote.quoteYear + " " + await getUsername(quote.quoteUser);
-
-        res.writeHead(200, {'Content-Length': formattedQuote.length, 'Content-Type': 'text/plain'});
-        res.end(formattedQuote);
-    });
-
-    server.listen(3453);
-
-    //TODO Implement caching
-}
-
-async function getApprovedQuotes(clientPool : MongoClient): Promise<Map<string, Quote>> {
-    let results = await (clientPool.db().collection("quotes").find({$and: [{"accepted": true}, {"guild": "264032838712688640"}]})).toArray(); //Search where accepted is true
-
-    let formattedResults: Map<string, Quote> = new Map<string, Quote>();
-    for (let i = 0; i !== results.length; i++) {
-        let currentQuote = Quote.modelBuilder(results[i]);
-        formattedResults.set(currentQuote.message, currentQuote);
+    constructor() {
+        this.discordTools = new DiscordTools();
+        this.databaseWrapper = new DatabaseWrapper();
     }
 
-    return formattedResults;
+    async init(){
+        await this.discordTools.loginClient();
+        await this.databaseWrapper.initPool();
+    }
+
+    startRestServer(){
+        this.server = http.createServer(async (req, res) => {
+            let quotes = await this.databaseWrapper.getQuotes();
+            let quote = quotes[getRandomNumber(0, quotes.length-1)]
+
+            let formattedQuote = quote.quoteText + " - " + quote.quoteYear + " " + await this.discordTools.getUsername(quote.quoteUser);
+            res.writeHead(200, {'Content-Length': formattedQuote.length, 'Content-Type': 'text/plain'});
+            res.end(formattedQuote);
+        });
+
+        this.server.listen(3453);
+        console.info("HTTP Server Started")
+    }
 }
 
-async function getUsername(id : string) : Promise <string>{
-    let user = await client.guilds.cache.get("264032838712688640").member(id).fetch();
-    return user.nickname;
-}
-
-function getRandomNumber(minimum: number, maximum: number): number {
-    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-}
-
-const client = new Discord.Client();
-client.login(properties.token).then(() => runner())
+let runner = new Runner();
+runner.init()
+    .then(() => runner.startRestServer())
+    .then(() => console.info("Started"))
+    .catch(error => console.log("Error while starting " + error));
